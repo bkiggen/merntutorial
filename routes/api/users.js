a19279d188a70795ab49f1c458dcf479614bd09c
Ben Kiggen <benkiggen@gmail.com>
2020-03-14 20:07:56 -0700

    update user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,21 +11,24 @@ router.post('/', [
   check('name', 'Name is Required').not().isEmpty(), 
   check('email', 'Please use a valid email').isEmail(), 
   check('password', 'Please enter a password with 6 or more characters!').isLength({ min: 6 })
-], (req, res) => {
+], 
+async (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res.status(400).json({ errors: errors.array() });
   }
   const { name, email, password } = req.body;
-  // See if the user exists
 
-  // Get users gravatar
+  try {
+    // See if the user exists
+    // Get users gravatar
+    // Encrypt password
+    // Return jsonwebtoken
+    res.send("User route");
+    } catch (err) {
 
-  // Encrypt password
-
-  // Return jsonwebtoken
-
-  res.send('User route')
+    }
+  
 });
 
 module.exports = router;
