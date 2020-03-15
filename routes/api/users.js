8d726ec6d35c273216b9435c1b78aef6a2c2b47b
Ben Kiggen <benkiggen@gmail.com>
2020-03-15 09:48:02 -0700

    add jwt auth checking

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const config = require('config');
 const User = require('../../models/User');
+
 
 // @route    POST api/users
 // @desc     Register user
@@ -20,11 +24,46 @@ async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
-    // See if the user exists
-    // Get users gravatar
+    let user = await User.findOne({ email });
+    
+    if(user) {
+      return res.status(400).json({ errors: [ { msg: 'User already exists!'}] });
+    }
+
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm'
+    });
+
+    user = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
     // Encrypt password
-    // Return jsonwebtoken
-    res.send("User route");
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload, 
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if(err) throw err;
+          res.json({ token });
+        } 
+        )
     } catch (err) {
 
     }
