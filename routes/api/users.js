ecc67175553da7c078e95883046b5da678863c16
Ben Kiggen <benkiggen@gmail.com>
2020-03-14 19:43:14 -0700

    update user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route    GET api/users
-// @desc     Test route
+const User = require('../../models/User');
+
+// @route    POST api/users
+// @desc     Register user
 // @access   Public 
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+  check('name', 'Name is Required').not().isEmpty(), 
+  check('email', 'Please use a valid email').isEmail(), 
+  check('password', 'Please enter a password with 6 or more characters!').isLength({ min: 6 })
+], (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() });
+  }
+  const { name, email, password } = req.body;
+  // See if the user exists
+
+  // Get users gravatar
+
+  // Encrypt password
+
+  // Return jsonwebtoken
+
+  res.send('User route')
+});
 
 module.exports = router;
